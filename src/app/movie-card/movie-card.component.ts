729344491ec1089c45419d8dcf12b080fcd5ee15
OBo1307 <och.bogdan@gmail.com>
2023-08-22 10:57:18 +0100

    added MovieCard

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { UserRegistrationService } from '../fetch-api-data.service';
 
+import { GenreDetailsComponent } from '../genre-details/genre-details.component';
+import { DirectorDetailsComponent } from '../director-details/director-details.component';
+import { MovieSynopsisComponent } from '../movie-synopsis/movie-synopsis.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
@@ -8,10 +14,50 @@ import { UserRegistrationService } from '../fetch-api-data.service';
 })
 export class MovieCardComponent {
   movies: any[] = [];
-  constructor(public fetchApiData: UserRegistrationService) { }
+  favorites: any[] = [];
+
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar
+    ) { }
 
   ngOnInit(): void {
     this.getMovies();
+    this.getFavoriteMovies();
+  }
+
+  getFavoriteMovies(): void {
+    this.fetchApiData.getUser().subscribe((resp: any) => {
+      this.favorites = resp.FavoriteMovies;
+      console.log(this.favorites);
+      return this.favorites;
+    });
+  }
+  isFavorite(id: string): boolean {
+    return this.favorites.includes(id);
+  }
+
+  addToFavorites(id: string): void {
+    console.log(id);
+    this.fetchApiData.addMovieToFavorites(id).subscribe((result) => {
+      console.log(result);
+      this.snackBar.open('Movie added to favorites', 'OK', {
+        duration: 4000,
+      });
+      this.ngOnInit();
+    });
+  }
+
+  deleteFromFavorites(id: string): void {
+    console.log(id);
+    this.fetchApiData.deleteMovieFromFavorites(id).subscribe((result) => {
+      console.log(result);
+      this.snackBar.open('Movie deleted from favorites', 'OK', {
+        duration: 2000,
+      });
+      this.ngOnInit();
+    });
   }
 
   getMovies(): void {
@@ -19,6 +65,45 @@ export class MovieCardComponent {
       this.movies = resp;
       console.log(this.movies);
       return this.movies;
+    });
+  }
+
+  openGenreDetails(name: string, description: string): void {
+    console.log(name);
+    this.dialog.open(GenreDetailsComponent, {
+      data: {
+        Name: name,
+        Description: description,
+      },
+    });
+  }
+
+  openDirectorDetails(name: string, bio: string, birth: string): void {
+    console.log(name);
+    this.dialog.open(DirectorDetailsComponent, {
+      data: {
+        Name: name,
+        Bio: bio,
+        Birth: birth,
+      },
+    });
+  }
+
+  openMovieSynopsis(
+    title: string,
+    movieDirector: string,
+    movieGenre: string,
+    movieDescription: string,
+    movieImagePath: string
+  ): void {
+    this.dialog.open(MovieSynopsisComponent, {
+      data: {
+        Title: title,
+        Director: movieDirector,
+        Genre: movieGenre,
+        Description: movieDescription,
+        Image: movieImagePath,
+      },
     });
   }
 }
